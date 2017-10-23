937c5272e14fdbae2930a8195de14ac5d0e5a362
Christoph Dorn <christoph@christophdorn.com>
2017-10-23 00:17:48 -0700

    initial settings panel UI via riotjs

--- src/settings.rep.js
+++ src/settings.rep.js
@@ -1,18 +1,32 @@
 
 exports.main = function (JSONREP, node) {
+    
+    return JSONREP.makeRep({
+        "config": {
+            "node": node
+        },
+        "code": (riotjs:makeRep () >>>
 
-    return JSONREP.makeRep(
-        (html () >>>
-            '<div class="panel.settings">' + 
-            'Settings' +
-            '</div>'
-        <<<),
-        {
-            on: {
-                mount: function (el) {
+            <div>
+            
+                <ul>
+                    <li><input type="checkbox" name="setting.enableUserAgentHeader"/> Enable UserAgent Header</li>
+                    <li><input type="checkbox" name="setting.enableFirePHPHeader"/> Enable FirePHP Header</li>
+                </ul>
+            </div>
 
-                }
-            }
-        }
-    );
+            <style>
+            </style>
+
+            <script>
+/*    
+                this.root.innerHTML = [
+                    this.opts.config.node.message,
+                    this.opts.config.append
+                ].join("");
+*/
+            </script>
+
+        <<<)
+    });
 };
