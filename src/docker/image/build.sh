860be065c5fe5ed492e485fa435567a4f5e5cf96
Christoph Dorn <christoph@christophdorn.com>
2017-11-18 19:47:32 -0800

    wip

--- src/docker/image/build.sh
+++ src/docker/image/build.sh
@@ -2,6 +2,8 @@
 
 echo -e "\n########## START BUILD ON DOCKER IMAGE ##########\n"
 
+export BO_VERSION_RECENT_NODE="9"
+export BO_VERSION_NVM_NODE="9"
 
 echo "Show info ..."
 
@@ -18,8 +20,12 @@ pushd "firephp"
 
     echo "Installing ..."
 
-    export VERBOSE=1
-    export BO_VERBOSE=1
+#    export VERBOSE=1
+#    export BO_VERBOSE=1
+
+#    echo "npm version: $(npm --version)"
+#    npm i -g npm@5.5.0
+#    echo "npm version: $(npm --version)"
 
     npm install
 
