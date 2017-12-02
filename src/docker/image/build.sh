d987510309fa4425c7927cdb7153b355bd07ae14
Christoph Dorn <christoph@christophdorn.com>
2017-12-01 21:18:18 -0800

    wip

--- src/docker/image/build.sh
+++ src/docker/image/build.sh
@@ -10,20 +10,14 @@ echo "Show info ..."
 echo "node version: $(node --version)"
 echo "npm version: $(npm --version)"
 
-echo "Cloning ..."
-
-git clone https://github.com/firephp/firephp-for-firefox-devtools.git firephp
-
 pushd "firephp"
-
-    git checkout amo
-
-    echo "Installing ..."
-
-    npm install --unsafe-perm
 
     echo "Building ..."
 
     npm run build
+
+    echo "### DIFF ###"
+
+    git diff
 
 popd
